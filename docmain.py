6d7292ebbc5e73f32166bc207a58577dbc4cd854
Mathieu Duponchelle <mathieu.duponchelle@opencreed.com>
2015-07-06 23:13:09 +0200

    Aggregation :)

--- docmain.py
+++ docmain.py
@@ -6,12 +6,40 @@ from giscanner import ast
 import logging
 
 class NameFormatter(object):
-    def __init__(self, language='C'):
+    def __init__(self, language='python'):
         if language == 'C':
             self.__make_node_name = self.__make_c_node_name
+        elif language == 'python':
+            self.__make_node_name = self.__make_python_node_name
 
     def get_full_node_name(self, node):
         return self.__make_node_name (node)
+
+    def __make_python_node_name (self, node):
+        out = ""
+        if type (node) in (ast.Namespace, ast.DocSection):
+            return node.name
+
+        if type (node) in (ast.Signal, ast.VFunction, ast.Property, ast.Field):
+            out = "%s.%s-%s" % (node.namespace.name, node.parent.name,
+                    node.name)
+
+        if type (node) == ast.Function:
+            while node:
+                if out:
+                    out = "%s.%s" % (node.name, out)
+                else:
+                    out = node.name
+
+                if hasattr (node, "parent"):
+                    node = node.parent
+                else:
+                    node = None
+
+        elif type (node) in (ast.Class, ast.Enum):
+            out = "%s.%s" % (node.namespace.name, node.name)
+
+        return out
 
     def __make_c_node_name (self, node):
         out = ""
@@ -133,15 +161,61 @@ def get_sorted_symbols_from_sections (sections, symbols):
         get_sorted_symbols_from_sections (element, symbols)
 
 
+class AggregatedClass(object):
+    def __init__(self, class_node, section_node):
+        self.class_node = class_node
+        self.__formatter = NameFormatter()
+        self.__section_node = section_node
+        self.signals = {}
+        self.methods = {}
+        self.properties = {}
+        self.virtual_functions = {}
+
+    def add_aggregated_node (self, node):
+        name = self.__formatter.get_full_node_name (node)
+        if type (node) == ast.Signal:
+            self.signals[name] = node
+        elif type (node) == ast.Function:
+            self.methods[name] = node
+        elif type (node) == ast.VFunction:
+            self.virtual_functions[name] = node
+        elif type (node) == ast.Property:
+            self.properties[name] = node
+        else:
+            raise NotImplementedError
+
+    def __sort_symbols (self, nodes):
+        sorted_symbols = []
+        for symbol in self.__section_node:
+            try:
+                node = nodes.pop (symbol.text)
+                sorted_symbols.append (node)
+            except KeyError:
+                continue
+        return sorted_symbols
+
+    def sort (self):
+        self.methods = self.__sort_symbols (self.methods)
+        self.signals = self.__sort_symbols (self.signals)
+        self.properties = self.__sort_symbols (self.properties)
+        self.virtual_functions = self.__sort_symbols (self.virtual_functions)
+
 class Renderer(object):
-    def __init__ (self, transformer, include_directories):
+    def __init__ (self, transformer, include_directories, sections,
+            do_class_aggregation=False):
         self.__transformer = transformer
         self.__include_directories = include_directories
+        self.__sections = sections
+        self.__do_class_aggregation = do_class_aggregation
 
         self.__handlers = self.__create_handlers ()
         self.__doc_renderers = self.__create_doc_renderers ()
         self.__doc_scanner = DocScanner()
         self.__name_formatter = NameFormatter()
+
+        # Used to aggregate the class with its members
+        self.__current_class = None
+        self.__aggregated_classes = []
 
         # Used to avoid parsing code as doc
         self.__processing_code = False
@@ -151,9 +225,42 @@ class Renderer(object):
 
     def render (self, output):
         self.__walk_node(output, self.__transformer.namespace, [])
-        self.__transformer.namespace.walk(lambda node, chain: self.__walk_node(output, node, chain))
+        self.__transformer.namespace.walk(lambda node, chain:
+                self.__walk_node(output, node, chain))
+        if self.__do_class_aggregation:
+            self.__aggregate_classes (output)
 
-    def render_index (self, output, sections):
+    def __render_section (self, name, output, elements):
+        if not elements:
+            return ""
+
+        out = ""
+        try:
+            out += self._render_section (name)
+        except AttributeError:
+            pass
+
+        for elem in elements:
+            with open (self.__make_file_name (output, elem), 'r') as f:
+                out += f.read()
+
+        return out
+
+    def __aggregate_classes (self, output):
+        for klass in self.__aggregated_classes:
+            klass.sort()
+            filename = self.__make_file_name (output, klass.class_node)
+            with open (filename, 'a') as f:
+                out = ""
+                out += self.__render_section ('Properties', output,
+                        klass.properties)
+                out += self.__render_section ('Methods', output, klass.methods)
+                out += self.__render_section ('Signals', output, klass.signals)
+                out += self.__render_section ('Virtual Functions', output,
+                        klass.virtual_functions)
+                f.write (out)
+
+    def render_index (self, output):
         out = ""
 
         try:
@@ -164,7 +271,7 @@ class Renderer(object):
         filenames = [os.path.splitext(os.path.basename (filename))[0] for
                 filename in self.__created_pages]
         try:
-            out += self._render_index (filenames, sections)
+            out += self._render_index (filenames, self.__sections)
         except AttributeError:
             pass
 
@@ -294,7 +401,7 @@ class Renderer(object):
 
         function_name = self.__name_formatter.get_full_node_name (func)
         try:
-            return self._render_function_call (name)
+            return self._render_function_call (function_name)
         except AttributeError:
             return ""
 
@@ -369,22 +476,58 @@ class Renderer(object):
     def __render_heading (self, node, match, props):
         raise NotImplementedError
 
-    def __walk_node(self, output, node, chain):
+    def __get_class_symbols (self, root, node):
+        formatter = NameFormatter ()
+        node_name = formatter.get_full_node_name (node)
+        return_next = False
+        for element in root:
+            if return_next:
+                return element
+
+            if element.text == node_name:
+                return_next = True
+                continue
+
+            res = self.__get_class_symbols (element, node)
+            if res is not None:
+                return res
+        return None
+
+    def __do_aggregation (self, node):
+        if type(node) == ast.Namespace:
+            return False
+
+        if type (node) == ast.Class:
+            symbol = self.__get_class_symbols (self.__sections, node)
+            self.__current_class = AggregatedClass (node, symbol)
+            self.__aggregated_classes.append (self.__current_class)
+            return False
+        elif self.__current_class and node.parent is self.__current_class.class_node:
+            self.__current_class.add_aggregated_node (node)
+            return True
+        else:
+            self.__current_class = None
+            return False
+
+    def __make_file_name (self, output, node):
         extension = self._get_extension ()
         name = self.__name_formatter.get_full_node_name (node)
+        return os.path.join (output, "%s.%s" % (name, extension))
 
-        filename = os.path.join (output, "%s.%s" % (name, extension))
+    def __walk_node(self, output, node, chain):
+        filename = self.__make_file_name (output, node)
+
         try:
             handler = self.__handlers[type (node)]
         except KeyError:
             return True
 
-        print name
-
         with open (filename, 'w') as f:
             f.write (handler (node))
 
-        self.__created_pages.append (filename)
+        if not self.__do_aggregation (node):
+            self.__created_pages.append (filename)
+
         return True
 
     def _get_extension (self):
@@ -565,6 +708,9 @@ class MarkdownRenderer (Renderer):
             prototype += param_name
         prototype += ")"
         return self.__render_title (prototype, level=3)
+
+    def _render_section (self, section_name):
+        return self.__render_title ("%s:" % section_name, level=2)
 
     def _end_function (self):
         return self._render_new_paragraph ()
@@ -808,6 +954,7 @@ def doc_main (args):
     else:
         sections = ET.parse (args.sections_file).getroot ()
 
-    renderer = SlateMarkdownRenderer (transformer, args.markdown_include_paths)
+    renderer = SlateMarkdownRenderer (transformer, args.markdown_include_paths,
+            sections, do_class_aggregation=True)
     renderer.render (args.output)
-    renderer.render_index (args.output, sections)
+    renderer.render_index (args.output)
