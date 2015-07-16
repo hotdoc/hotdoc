02660445fac695fbd83a24152494d842be3b81d6
Mathieu Duponchelle <mathieu.duponchelle@opencreed.com>
2015-07-16 17:32:22 +0200

    translate_sections: Actually keep TITLE

--- translate_sections.sh
+++ translate_sections.sh
@@ -2,7 +2,7 @@
 
 echo "<SECTIONS>" > raw.txt
 
-cat $1 | grep -v "^\s*#\|<INCLUDE>\|</INCLUDE>\|<FILE>\|</FILE>\|<SUBSECTION.*>\|</SUBSECTION>" | sed "s/<TITLE>/<SYMBOL>/g" | sed "s/<\/TITLE>/<\/SYMBOL>\n<SYMBOLS>/" | sed "s/<\/SECTION>/<\/SYMBOLS>\n<\/SECTION>/g" | awk '!/^\s*(<|$)/{print "<SYMBOL>"$0"</SYMBOL>";next}1' >> raw.txt
+cat $1 | grep -v "^\s*#\|<INCLUDE>\|</INCLUDE>\|<FILE>\|</FILE>\|<SUBSECTION.*>\|</SUBSECTION>" | sed "s/<\/TITLE>/<\/TITLE>\n<SYMBOLS>/" | sed "s/<\/SECTION>/<\/SYMBOLS>\n<\/SECTION>/g" | awk '!/^\s*(<|$)/{print "<SYMBOL>"$0"</SYMBOL>";next}1' >> raw.txt
 
 echo "</SECTIONS>" >> raw.txt
 
