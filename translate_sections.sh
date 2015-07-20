fa989c37103dc951abd2aec614e79576bacf66e1
Mathieu Duponchelle <mathieu.duponchelle@opencreed.com>
2015-07-20 06:05:48 +0200

    Don't translate sections so much

--- translate_sections.sh
+++ translate_sections.sh
@@ -2,8 +2,8 @@
 
 echo "<SECTIONS>" > raw.txt
 
-cat $1 | grep -v "^\s*#\|<INCLUDE>\|</INCLUDE>\|<FILE>\|</FILE>\|<SUBSECTION.*>\|</SUBSECTION>" | sed "s/<\/TITLE>/<\/TITLE>\n<SYMBOLS>/" | sed "s/<\/SECTION>/<\/SYMBOLS>\n<\/SECTION>/g" | awk '!/^\s*(<|$)/{print "<SYMBOL>"$0"</SYMBOL>";next}1' >> raw.txt
+cat $1 | grep -v "^<SUBSECTION.*>\|</SUBSECTION>" >> raw.txt
 
 echo "</SECTIONS>" >> raw.txt
 
-xmllint --format raw.txt > $2 && rm raw.txt
+mv raw.txt $2
