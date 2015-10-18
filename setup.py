07845f8eb5e1a44a696d27b61229e8a114d110e2
Mathieu Duponchelle <mathieu.duponchelle@opencreed.com>
2015-10-18 03:44:22 +0200

    0.5 release
    This release marks our first significant contributor, yay \o/
    
    Features of this release:
    
    * Ongoing hardening of the code
    * Code examples syntax highlighting
    * Refactoring of the source frontend
    * Markdown inclusion with {{ }}
    * Extension to handle dbus sources using dbus-deviation
    * Introduction of a test project, available at https://github.com/MathieuDuponchelle/test_hotdoc
    
    Regression in this release:
    
    * Incremental building has been removed for now, its design
      was a bit awkward and its implementation was not bullet proof.
      There are plans for online editing of the documentation in the works,
      and I expect this to overlap with a new design (I'll probably implement
      an intermediate format, not really sure about the design yet)
    
    Plan for the next cycle:
    
    * Self-document through the implementation of a python frontend =)
    * Pick and implement enhancement requests (https://github.com/MathieuDuponchelle/hotdoc/issues?q=is%3Aopen+is%3Aissue+label%3Aenhancement)
    
    That's all.

--- setup.py
+++ setup.py
@@ -138,7 +138,7 @@ c_comment_scanner_module = FlexExtension(
                             'hotdoc/lexer_parsers/c_comment_scanner/scanner.h'])
 
 setup(name='hotdoc',
-      version='0.4.9',
+      version='0.5',
       description='A documentation tool based on clang',
       keywords='documentation gnome clang doxygen',
       url='https://github.com/MathieuDuponchelle/hotdoc',
@@ -162,8 +162,8 @@ setup(name='hotdoc',
        'hotdoc/transition_scripts/sgml_to_sections.py',
        'hotdoc/transition_scripts/translate_sections.sh'],
       package_data = {
-          'hotdoc.formatters.html': ['templates/*', 'style.css',
-              'redstyle.css', 'greenstyle.css', 'home.png', 'API_index.js'],
+          'hotdoc.formatters.html': ['templates/*', '*.css',
+              'home.png', '*.js'],
           'hotdoc.extensions': ['templates/*'],
           },
       install_requires = ['wheezy.template',
