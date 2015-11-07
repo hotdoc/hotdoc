caf593bd13773008e099bcea641b1f56f1d8b329
Mathieu Duponchelle <mathieu.duponchelle@opencreed.com>
2015-11-07 20:04:54 +0100

    c_extension: Basic cleanup

--- hotdoc/extensions/c_extension.py
+++ hotdoc/extensions/c_extension.py
@@ -1,13 +1,11 @@
-import os, ast, linecache
+import os, linecache
 
 import clang.cindex
 from ctypes import *
 from fnmatch import fnmatch
 
-from datetime import datetime
-
 from hotdoc.core.base_extension import BaseExtension
-from hotdoc.utils.loggable import Loggable, progress_bar
+from hotdoc.utils.loggable import Loggable
 from hotdoc.core.symbols import *
 from hotdoc.core.comment_block import comment_from_tag
 from hotdoc.lexer_parsers.c_comment_scanner.c_comment_scanner import get_comments
@@ -39,28 +37,14 @@ class ClangScanner(Loggable):
         self.full_scan = full_scan
         self.full_scan_patterns = full_scan_patterns
 
+        # FIXME: er maybe don't do that ?
         args = ["-isystem/usr/lib/clang/3.5.0/include/", "-Wno-attributes"]
         args.extend (options)
 
         self.symbols = {}
-        self.new_symbols = {}
-
-        self.external_symbols = {}
         self.comments = {}
-        self.token_groups = []
-        self.tus = []
-        self.__total_files = len (self.filenames)
-        self.__total_files_parsed = 0
-
-        self.__progress_bar = progress_bar.get_progress_bar ()
-        if self.__progress_bar is not None:
-            self.__progress_bar.set_header("Parsing sources (1 / 2)")
-            self.__progress_bar.clear()
-            self.__update_progress()
-
         self.parsed = set({})
 
-        n = datetime.now()
         if not full_scan:
             for filename in filenames:
                 with open (filename, 'r') as f:
@@ -71,15 +55,13 @@ class ClangScanner(Loggable):
                         if incremental:
                             self.__update_symbol_comment(block)
 
-        n = datetime.now()
         for filename in self.filenames:
-            if os.path.abspath(filename) in self.parsed:
+            if filename in self.parsed:
                 continue
 
             do_full_scan = any(fnmatch(filename, p) for p in full_scan_patterns)
             if do_full_scan:
                 tu = index.parse(filename, args=args, options=flags)
-                self.tus.append (tu)
                 for diag in tu.diagnostics:
                     if diag.severity <= 2 and str(diag.location.file) not in self.filenames:
                         self.warning ("Clang issue : %s" % str (diag))
@@ -89,40 +71,21 @@ class ClangScanner(Loggable):
                 self.__parse_file (filename, tu)
                 for include in tu.get_includes():
                     self.__parse_file (os.path.abspath(str(include.include)), tu)
-            else:
-                self.__total_files_parsed += 1
-                self.__update_progress ()
 
-        self.info ("%d internal symbols found" % len (self.symbols))
-        self.info ("%d external symbols found" % len (self.external_symbols))
+        self.info ("%d symbols found" % len (self.symbols))
         self.info ("%d comments found" % len (self.comments))
-
-    def __update_progress (self):
-        if self.__progress_bar is None:
-            return
-
-        if self.__total_files == 0:
-            return
-
-        percent = float (self.__total_files_parsed) / float (self.__total_files)
-        self.__progress_bar.update (percent, "%d / %d" %
-                (self.__total_files_parsed, self.__total_files)) 
 
     def __parse_file (self, filename, tu):
         if filename in self.parsed:
             return
 
-        self.parsed.add (os.path.abspath(filename))
+        self.parsed.add (filename)
         start = tu.get_location (filename, 0)
         end = tu.get_location (filename, os.path.getsize(filename))
         extent = clang.cindex.SourceRange.from_locations (start, end)
         cursors = self.__get_cursors(tu, extent)
         if filename in self.filenames:
-            self.__total_files_parsed += 1
-            self.__update_progress ()
-            self.find_internal_symbols (cursors, tu)
-        #else:
-        #    self.find_external_symbols (cursors, tu)
+            self.__create_symbols (cursors, tu)
 
     # That's the fastest way of obtaining our ast nodes for a given filename
     def __get_cursors (self, tu, extent):
@@ -137,14 +100,13 @@ class ClangScanner(Loggable):
         if count < 1:
             return
 
-        self.token_groups.append(clang.cindex.TokenGroup(tu, tokens_memory, tokens_count))
         cursors = (clang.cindex.Cursor * count)()
         clang.cindex.conf.lib.clang_annotateTokens (tu, tokens_memory, tokens_count,
                 cursors)
 
         return cursors
 
-    def find_internal_symbols(self, nodes, tu):
+    def __create_symbols(self, nodes, tu):
         for node in nodes:
             node._tu = tu
             if node.kind in [clang.cindex.CursorKind.FUNCTION_DECL,
@@ -161,11 +123,10 @@ class ClangScanner(Loggable):
                         self.doc_tool.raw_comment_parser.parse_comment \
                                 (node.raw_comment, str(node.location.file), 0)
 
-                self.symbols[node.spelling] = node
-                self.debug ("Found internal symbol [%s] of kind %s at location %s" %
+                self.debug ("Found symbol [%s] of kind %s at location %s" %
                         (node.spelling, str(node.kind), str (node.location)))
 
-            if node.spelling in self.new_symbols:
+            if node.spelling in self.symbols:
                 continue
 
             sym = None
@@ -179,7 +140,7 @@ class ClangScanner(Loggable):
                 sym = self.__create_typedef_symbol (node)
 
             if sym is not None:
-                self.new_symbols[node.spelling] = sym
+                self.symbols[node.spelling] = sym
 
     def __apply_qualifiers (self, type_, tokens):
         if type_.is_const_qualified():
@@ -208,14 +169,12 @@ class ClangScanner(Loggable):
         tokens.reverse()
         return tokens
 
-    def tokens_from_tokens (self, tokens):
-        toks = []
-
     def __update_symbol_comment(self, comment):
         esym = self.doc_tool.get_symbol (comment.name)
         if esym:
             esym.comment = comment
 
+    # FIXME: this belongs in doc_tool
     def __get_comment (self, comment_name):
         comment = self.comments.get(comment_name)
         if not comment:
@@ -228,7 +187,7 @@ class ClangScanner(Loggable):
         parameters = []
 
         if comment:
-            return_tag = comment.tags.pop('returns', None) 
+            return_tag = comment.tags.get('returns')
             return_comment = comment_from_tag(return_tag)
         else:
             return_comment = None
@@ -377,6 +336,7 @@ class ClangScanner(Loggable):
             else:
                 member_comment = None
             member_value = member.enum_value
+            # FIXME: this is pretty much a macro symbol ?
             member = self.doc_tool.get_or_create_symbol(Symbol, comment=member_comment, name=member.spelling,
                     filename=str(member.location.file),
                     lineno=member.location.line)
@@ -411,7 +371,7 @@ class ClangScanner(Loggable):
     def __create_function_macro_symbol (self, node, comment, original_text): 
         return_value = None
         if comment:
-            return_tag = comment.tags.pop ('returns', None)
+            return_tag = comment.tags.get ('returns')
             if return_tag:
                 return_comment = comment_from_tag (return_tag)
                 return_value = ReturnValueSymbol (comment=return_comment)
@@ -456,7 +416,7 @@ class ClangScanner(Loggable):
         parameters = []
 
         if comment:
-            return_tag = comment.tags.pop('returns', None) 
+            return_tag = comment.tags.get('returns')
             return_comment = comment_from_tag(return_tag)
         else:
             return_comment = None
@@ -499,43 +459,6 @@ class ClangScanner(Loggable):
                 lineno=node.location.line)
         return sym
 
-    def find_external_symbols(self, nodes, tu):
-        for node in nodes:
-            node._tu = tu
-            if node.kind == clang.cindex.CursorKind.TYPEDEF_DECL:
-                self.external_symbols[node.spelling] = node
-                self.debug ("Found external symbol %s" % node.spelling)
-
-                sym = self.__create_typedef_symbol (node)
-
-                if sym is not None:
-                    self.new_symbols[node.spelling] = sym
-
-    def lookup_ast_node (self, name):
-        return self.symbols.get(name) or self.external_symbols.get(name)
-
-    def lookup_underlying_type (self, name):
-        ast_node = self.lookup_ast_node (name)
-        if not ast_node:
-            return None
-
-        while ast_node.kind == clang.cindex.CursorKind.TYPEDEF_DECL:
-            t = ast_node.underlying_typedef_type
-            ast_node = t.get_declaration()
-        return ast_node.kind
-
-    def finalize (self):
-        """
-        FIXME: We need to cleanup before __del__ because
-        clang binding destructors rely on module level
-        global variables, which destruction order is undefined
-        at termination time ...
-        """
-        del self.token_groups
-        del self.symbols
-        del self.external_symbols
-        del self.tus
-
 class CExtension(BaseExtension):
     EXTENSION_NAME = 'c-extension'
 
@@ -546,15 +469,14 @@ class CExtension(BaseExtension):
         self.clang_options = args.clang_options
 
     def setup(self):
-        incremental = (self.stale_source_files != self.sources)
         self.scanner = ClangScanner(self.doc_tool, self.stale_source_files, self.clang_options, False,
-                ['*.h'], incremental)
+                ['*.h'], self.doc_tool.incremental)
 
     def get_source_files(self):
         return self.sources
 
     def get_extra_symbols(self):
-        return self.scanner.new_symbols
+        return self.scanner.symbols
 
     def get_comments(self):
         return self.scanner.comments
