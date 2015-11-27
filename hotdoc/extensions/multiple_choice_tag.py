cfb643ba03448f6dd58f289b2d633a1cf5960c6b
Mathieu Duponchelle <mathieu.duponchelle@opencreed.com>
2015-11-27 22:39:47 +0100

    TagExtension: implement choices blacklist

--- hotdoc/extensions/multiple_choice_tag.py
+++ hotdoc/extensions/multiple_choice_tag.py
@@ -1,5 +1,6 @@
 from hotdoc.core.base_extension import BaseExtension
 from hotdoc.core.comment_block import TagValidator
+from hotdoc.core.symbols import *
 
 MULTIPLE_CHOICE_DESCRIPTION=\
 """
@@ -29,6 +30,11 @@ class MultipleChoiceTagValidator(TagValidator):
             return True
 
         return value in self.choices
+
+class MultipleChoiceTagBlackList (object):
+    def __init__(self, name, choices):
+        self.name = name
+        self.choices = choices
 
 DESCRIPTION=\
 """
@@ -74,18 +80,73 @@ def validate_prototypes(wizard, prototypes):
 
     return True
 
+def parse_choice_blacklist(blacklist):
+    split = blacklist.split(':')
+    if len (split) != 2:
+        print "Invalid syntax for choices blacklist %s" % blacklist
+        return None
+
+    name = split[0]
+    if not name:
+        print "Invalid choices blacklist, missing name : %s" % blacklist
+        return None
+
+    choices = [choice for choice in split[1].split(',') if choice]
+
+    if not choices:
+        print "Invalid choices blacklist, missing choices : %s" % blacklist
+        return None
+
+    return MultipleChoiceTagBlackList(name, choices)
+
 class TagExtension(BaseExtension):
     EXTENSION_NAME='core-tags'
 
     def __init__(self, doc_tool, args):
         BaseExtension.__init__(self, doc_tool, args)
+
+        self.blacklists = []
+
         tag_prototypes = args.get('tag_prototypes', [])
+
         for prototype in tag_prototypes:
             validator = validator_from_prototype(prototype)
             if validator:
                 self.info ("Registering multiple choice tag %s with choices %s and default %s"
                         % (validator.name, validator.choices, validator.default))
-            doc_tool.register_tag_validator(validator)
+                doc_tool.register_tag_validator(validator)
+
+        blacklist_prototypes = args.get('choices_blacklist', [])
+
+        for prototype in blacklist_prototypes:
+            blacklist = parse_choice_blacklist(prototype)
+            if not blacklist:
+                continue
+
+            validator = doc_tool.tag_validators.get (blacklist.name)
+            if not validator:
+                "Blacklist for name %s applies to no known tags" % blacklist.name
+                continue
+
+            self.blacklists.append(blacklist)
+
+    def setup(self):
+        for extension in self.doc_tool.extensions.values():
+            formatter = extension.get_formatter(self.doc_tool.output_format)
+            formatter.formatting_symbol_signals[Symbol].connect(self.__formatting_symbol)
+
+    def __formatting_symbol(self, symbol):
+        if isinstance(symbol, QualifiedSymbol):
+            return True
+
+        for blacklist in self.blacklists:
+            tag = symbol.comment.tags.get(blacklist.name)
+            if not tag:
+                continue
+            if tag.description in blacklist.choices:
+                return False
+
+        return True
 
     @staticmethod
     def add_arguments(parser):
@@ -96,3 +157,6 @@ class TagExtension(BaseExtension):
                 help="Define multiple choice tags",
                 nargs='+', dest='tag_prototypes',
                 validate_function=validate_prototypes)
+        group.add_argument('--choices-blacklist', action="store",
+                help="filter out symbols based on their tags",
+                nargs='+', dest='choices_blacklist')
