3cfd52614ac16442a1c3c2991e688b54990fefca
Mathieu Duponchelle <mathieu.duponchelle@opencreed.com>
2015-12-08 08:41:12 +0100

    patcher: handle encodings correctly

--- hotdoc/transition_scripts/patcher.py
+++ hotdoc/transition_scripts/patcher.py
@@ -1,10 +1,15 @@
 import os
 import pygit2 as git
+import codecs
+import commands
 
 class Patcher(object):
     def patch (self, filename, begin, end, new_comment):
-        with open (filename, 'r') as f:
+        file_encoding = commands.getoutput('file -b --mime-encoding %s' %
+                filename)
+        with codecs.open(filename, 'r', file_encoding) as f:
             lines = f.readlines()
+
         res = lines[0:begin] + [new_comment + '\n'] + lines[end:]
         res = ''.join(res)
         with open (filename, 'w') as f:
