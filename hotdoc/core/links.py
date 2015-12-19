14f0999f8756c30cda9f7e18fcaa0546dea9a427
Mathieu Duponchelle <mathieu.duponchelle@opencreed.com>
2015-12-19 02:13:42 +0100

    Fix gi-specific code in links.

--- hotdoc/core/links.py
+++ hotdoc/core/links.py
@@ -1,13 +1,33 @@
 from hotdoc.core.alchemy_integration import MutableObject
+from hotdoc.utils.simple_signals import Signal
 
 class Link (MutableObject):
+    resolving_link_signal = Signal()
+    resolving_title_signal = Signal()
     def __init__(self, ref, title, id_):
-        self.title = title
+        self._title = title
         self.ref = ref
         self.id_ = id_
         MutableObject.__init__(self)
 
+    @property
+    def title(self):
+        resolved_title = Link.resolving_title_signal(self)
+        resolved_title = [elem for elem in resolved_title if elem is not
+                None]
+        if resolved_title:
+            return resolved_title[0]
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        self._title = value
+
     def get_link (self):
+        resolved_ref = Link.resolving_link_signal(self)
+        resolved_ref = [elem for elem in resolved_ref if elem is not None]
+        if resolved_ref:
+            return resolved_ref[0]
         return self.ref
 
 class LinkResolver(object):
@@ -15,31 +35,19 @@ class LinkResolver(object):
         self.__links = {}
         self.__external_links = {}
         self.doc_tool = doc_tool
-        self.__translated_names = {}
-
-    # Kind of gi-specific, but can't really hurt
-    def set_translated_names(self, names):
-        self.__translated_names = names
-
-    def translate(self, link):
-        if not link:
-            return None
-
-        link.title = self.__translated_names.get(link.id_, link.title)
-        return link
 
     def get_named_link (self, name):
         if name in self.__links:
-            return self.translate(self.__links[name])
+            return self.__links[name]
 
         sym = self.doc_tool.get_symbol(name)
         if sym:
             self.__links[name] = sym.link
-            return self.translate(sym.link)
+            return sym.link
 
         if name in self.__external_links:
             self.__links[name] = self.__external_links[name]
-            return self.translate(self.__links[name])
+            return self.__links[name]
 
         self.__links[name] = None
         return None
