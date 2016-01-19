76ce92fbae1aa5caa23e4869fef037977929e71d
Mathieu Duponchelle <mathieu.duponchelle@opencreed.com>
2016-01-19 22:24:16 +0100

    Setup pre-commit hooks

--- setup.py
+++ setup.py
@@ -1,26 +1,25 @@
-from setuptools import setup, find_packages
-from pkg_resources import parse_version as V
+import os
+import shutil
+import subprocess
+import tarfile
 from distutils.command.build import build
 from distutils.core import Command
-from setuptools.command.develop import develop
-from setuptools.command.sdist import sdist
-from setuptools.command.bdist_egg import bdist_egg
-
-import shutil
-import os
-import tarfile
-import subprocess
 
 from hotdoc.utils.setup_utils import VersionList
-
+from pkg_resources import parse_version as V
+from setuptools import find_packages, setup
+from setuptools.command.bdist_egg import bdist_egg
+from setuptools.command.develop import develop
+from setuptools.command.sdist import sdist
 
 pygit2_version = None
 try:
     libgit2_version = subprocess.check_output(['pkg-config', '--modversion',
-        'libgit2']).strip()
+                                               'libgit2']).strip()
     known_libgit2_versions = VersionList([V('0.22.0'), V('0.23.0')])
     try:
-        known_libgit2_version = known_libgit2_versions.find_le(V(libgit2_version))
+        known_libgit2_version = known_libgit2_versions.find_le(
+            V(libgit2_version))
 
         if known_libgit2_version == V('0.22.0'):
             pygit2_version = '0.22.1'
@@ -42,6 +41,11 @@ except subprocess.CalledProcessError as e:
 
 source_dir = os.path.abspath('./')
 
+
+DEFAULT_THEME =\
+        'https://people.collabora.com/~meh/hotdoc_bootstrap_theme/dist.tgz'
+
+
 class DownloadDefaultTemplate(Command):
     user_options = []
     description = "Download default html template"
@@ -55,9 +59,9 @@ class DownloadDefaultTemplate(Command):
     def run(self):
         import requests
         response = \
-                requests.get('https://people.collabora.com/~meh/hotdoc_bootstrap_theme/dist.tgz')
+            requests.get(DEFAULT_THEME)
 
-        with open ('default_theme.tgz', 'wb') as f:
+        with open('default_theme.tgz', 'wb') as f:
             f.write(response.content)
 
         tar = tarfile.open('default_theme.tgz')
@@ -75,28 +79,69 @@ class DownloadDefaultTemplate(Command):
 
         os.unlink('default_theme.tgz')
 
+
+def symlink(source, link_name):
+    import os
+    os_symlink = getattr(os, "symlink", None)
+    if callable(os_symlink):
+        os_symlink(source, link_name)
+    else:
+        import ctypes
+        csl = ctypes.windll.kernel32.CreateSymbolicLinkW
+        csl.argtypes = (ctypes.c_wchar_p, ctypes.c_wchar_p, ctypes.c_uint32)
+        csl.restype = ctypes.c_ubyte
+        flags = 1 if os.path.isdir(source) else 0
+        if csl(link_name, source, flags) == 0:
+            raise ctypes.WinError()
+
+
+class LinkPreCommitHook(Command):
+    user_options = []
+    description = "Create links for the style checking pre-commit hooks"
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        symlink(os.path.join(source_dir, 'pre-commit'),
+                os.path.join(source_dir, '.git', 'hooks', 'pre-commit'))
+
+
 class CustomDevelop(develop):
+
     def run(self):
         self.run_command('download_default_template')
+        self.run_command('link_pre_commit_hook')
         return develop.run(self)
 
+
 class CustomBuild(build):
+
     def run(self):
         self.run_command('download_default_template')
         return build.run(self)
 
+
 class CustomSDist(sdist):
+
     def run(self):
         self.run_command('download_default_template')
         return sdist.run(self)
 
+
 class CustomBDistEgg(bdist_egg):
+
     def run(self):
         self.run_command('download_default_template')
         return bdist_egg.run(self)
 
 install_requires = [
     'cffi',
+    'git-pylint-commit-hook',
+    'git-pep8-commit-hook',
     'wheezy.template==0.1.167',
     'CommonMark==0.6.1',
     'cmarkpy==0.1.3',
@@ -109,31 +154,32 @@ if pygit2_version is not None:
     install_requires.append('pygit2==%s' % pygit2_version)
 
 setup(name='hotdoc',
-        version='0.6.6',
-        description='A documentation tool micro-framework',
-        keywords='documentation',
-        url='https://github.com/hotdoc/hotdoc',
-        author='Mathieu Duponchelle',
-        author_email='mathieu.duponchelle@opencreed.com',
-        license='LGPL',
-        packages=find_packages(),
+      version='0.6.6',
+      description='A documentation tool micro-framework',
+      keywords='documentation',
+      url='https://github.com/hotdoc/hotdoc',
+      author='Mathieu Duponchelle',
+      author_email='mathieu.duponchelle@opencreed.com',
+      license='LGPL',
+      packages=find_packages(),
 
-        # Only fancy thing in there now, we want to download a
-        # a default theme and bower is shitty.
-        cmdclass = {'build': CustomBuild,
-            'sdist': CustomSDist,
-            'develop': CustomDevelop,
-            'bdist_egg': CustomBDistEgg,
-            'download_default_template': DownloadDefaultTemplate},
-        scripts = ['hotdoc/hotdoc'],
-        package_data = {
-            'hotdoc.formatters.html': ['templates/*', 'assets/*'],
-            'hotdoc': ['default_theme/templates/*',
-                'default_theme/js/*',
-                'default_theme/css/*',
-                'default_theme/fonts/*'],
-            },
-        install_requires = install_requires,
-        setup_requires = ['cffi',
-            'requests'],
-        )
+      # Only fancy thing in there now, we want to download a
+      # a default theme and bower is shitty.
+      cmdclass={'build': CustomBuild,
+                   'sdist': CustomSDist,
+                  'develop': CustomDevelop,
+                'bdist_egg': CustomBDistEgg,
+                'link_pre_commit_hook': LinkPreCommitHook,
+                'download_default_template': DownloadDefaultTemplate},
+      scripts=['hotdoc/hotdoc'],
+      package_data={
+          'hotdoc.formatters.html': ['templates/*', 'assets/*'],
+          'hotdoc': ['default_theme/templates/*',
+                     'default_theme/js/*',
+                     'default_theme/css/*',
+                     'default_theme/fonts/*'],
+      },
+      install_requires=install_requires,
+      setup_requires=['cffi',
+                      'requests'],
+      )
