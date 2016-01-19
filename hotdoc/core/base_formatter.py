7eea9994bd88a274af9c85ad8fc7f79f40ebae23
Mathieu Duponchelle <mathieu.duponchelle@opencreed.com>
2016-01-19 23:26:07 +0100

    base_formatter: pass through pylint and pep8

--- hotdoc/core/base_formatter.py
+++ hotdoc/core/base_formatter.py
@@ -1,56 +1,60 @@
 # -*- coding: utf-8 -*-
 
+"""
+This module defines a base Formatter class
+"""
+
 import os
 import shutil
 from collections import defaultdict
 from xml.sax.saxutils import unescape
 
 import pygraphviz as pg
-from hotdoc.core.symbols import *
+from hotdoc.core.symbols import QualifiedSymbol
 from hotdoc.utils.simple_signals import Signal
 from hotdoc.utils.utils import recursive_overwrite
 
 
-def all_subclasses(cls):
-        return cls.__subclasses__() + [g for s in cls.__subclasses__()
-                                       for g in all_subclasses(s)]
+def _create_hierarchy_graph(hierarchy):
+    """
+    Utility function
+    """
+    # FIXME: handle multiple inheritance
+    graph = pg.AGraph(directed=True, strict=True)
+
+    for pair in hierarchy:
+        parent_link = pair[0].get_type_link()
+        child_link = pair[1].get_type_link()
+
+        graph.add_node(parent_link.title, URL=parent_link.get_link(),
+                       style="rounded", shape="box")
+        graph.add_node(child_link.title, URL=child_link.get_link(),
+                       style="rounded", shape="box")
+
+        graph.add_edge(parent_link.title, child_link.title)
+
+    return graph
+
 
 class Formatter(object):
+    """
+    The base Formatter class
+    """
     formatting_page_signal = Signal()
 
-    def __init__ (self, doc_tool):
+    def __init__(self, doc_tool):
         self.doc_tool = doc_tool
         self._output = doc_tool.output
 
         self.formatting_symbol_signal = Signal()
         self.current_page = None
 
-    def _create_hierarchy_graph (self, hierarchy):
-        # FIXME: handle multiple inheritance
-        graph = pg.AGraph(directed=True, strict=True)
-
-        for pair in hierarchy:
-            parent_link = pair[0].get_type_link()
-            child_link = pair[1].get_type_link()
-
-            graph.add_node(parent_link.title, URL=parent_link.get_link(),
-                    style="rounded", shape="box")
-            graph.add_node(child_link.title, URL=child_link.get_link(),
-                    style="rounded", shape="box")
-
-            graph.add_edge (parent_link.title, child_link.title)
-
-        return graph
-
-    def patch_page(self, page, symbol):
-        raise NotImplementedError
-
-    def format_symbol (self, symbol):
+    def __format_symbol(self, symbol):
         self.__format_symbols(symbol.get_children_symbols())
 
         # We only need to resolve qualified symbols now because
         # they're referring to an actual type, not referred to.
-        if isinstance (symbol, QualifiedSymbol):
+        if isinstance(symbol, QualifiedSymbol):
             symbol.resolve_links(self.doc_tool.link_resolver)
 
         res = self.formatting_symbol_signal(symbol)
@@ -58,39 +62,43 @@ class Formatter(object):
         if False in res:
             return False
 
-        symbol.formatted_doc = self.__format_doc (symbol.comment)
-        out, standalone = self._format_symbol (symbol)
+        symbol.formatted_doc = self.__format_doc(symbol.comment)
+        out, standalone = self._format_symbol(symbol)
         symbol.detailed_description = out
 
         # FIXME: figure out whether this approach is desirable
         if standalone:
-            self._write_page (symbol)
+            self.write_page(symbol)
 
         return True
 
-    def emit_formatting_page(self, page):
+    def _emit_formatting_page(self, page):
         Formatter.formatting_page_signal(self, page)
 
-    def format (self, page):
-        self.__format_page (page)
-        self._copy_extra_files ()
+    def format(self, page):
+        """
+        Formats a given page and its subpages
+        """
+        self.__format_page(page)
+        self._copy_extra_files()
 
     def __format_symbols(self, symbols):
         for symbol in symbols:
             if symbol is None:
                 continue
-            symbol.skip = not self.format_symbol(symbol)
+            symbol.skip = not self.__format_symbol(symbol)
 
-    def __format_page (self, page):
+    def __format_page(self, page):
         self.current_page = page
 
         if page.is_stale:
             page.output_attrs = defaultdict(lambda: defaultdict(dict))
-            self.emit_formatting_page(page)
+            self._emit_formatting_page(page)
             self.doc_tool.update_doc_parser(page.extension_name)
             self.__format_symbols(page.symbols)
-            page.detailed_description = self.doc_tool.formatter._format_page (page)[0]
-            self.doc_tool.formatter._write_page (page)
+            page.detailed_description =\
+                self.doc_tool.formatter.format_page(page)[0]
+            self.doc_tool.formatter.write_page(page)
 
         for pagename in page.subpages:
             cpage = self.doc_tool.doc_tree.get_page(pagename)
@@ -102,43 +110,64 @@ class Formatter(object):
                 self.doc_tool.formatter = formatter
                 self.doc_tool.formatter.format(cpage)
             else:
-                self.__format_page (cpage)
+                self.__format_page(cpage)
             self.doc_tool.formatter = self
 
-
-    def _copy_extra_files (self):
+    def _copy_extra_files(self):
         asset_path = self.doc_tool.get_assets_path()
-        if not os.path.exists (asset_path):
-            os.mkdir (asset_path)
+        if not os.path.exists(asset_path):
+            os.mkdir(asset_path)
 
-        for f in self._get_extra_files():
-            basename = os.path.basename (f)
-            path = os.path.join (asset_path, basename)
-            if os.path.isfile(f):
-                shutil.copy (f, path)
-            elif os.path.isdir(f):
-                recursive_overwrite(f, path)
+        for _ in self._get_extra_files():
+            basename = os.path.basename(_)
+            path = os.path.join(asset_path, basename)
+            if os.path.isfile(_):
+                shutil.copy(_, path)
+            elif os.path.isdir(_):
+                recursive_overwrite(_, path)
 
-    def _write_page (self, page):
-        path = os.path.join (self._output, page.link.ref)
-        with open (path, 'w') as f:
+    def write_page(self, page):
+        """
+        Banana banana
+        """
+        path = os.path.join(self._output, page.link.ref)
+        with open(path, 'w') as _:
             out = page.detailed_description
-            f.write (out.encode('utf-8'))
+            _.write(out.encode('utf-8'))
 
-    def _format_doc_string (self, docstring):
+    def _format_doc_string(self, docstring):
         if not docstring:
             return ""
 
-        out = ""
-        docstring = unescape (docstring)
-        rendered_text = self.doc_tool.doc_parser.translate (docstring, 'html')
+        docstring = unescape(docstring)
+        rendered_text = self.doc_tool.doc_parser.translate(docstring, 'html')
         return rendered_text
 
-    def __format_doc (self, comment):
+    def patch_page(self, page, symbol):
+        """
+        Subclasses should implement this in order to allow
+        live patching of the documentation.
+        """
+        raise NotImplementedError
+
+    def format_page(self, page):
+        """
+        Banana banana
+        """
+        raise NotImplementedError
+
+    def _format_symbol(self, symbol):
+        """
+        Banana banana
+        """
+        raise NotImplementedError
+
+    def __format_doc(self, comment):
         out = ""
         if comment and comment.description:
-            out = self.doc_tool.doc_parser.translate_comment (comment, 'html')
+            out = self.doc_tool.doc_parser.translate_comment(comment, 'html')
         return out
 
-    def _get_extra_files (self):
+    # pylint: disable=no-self-use
+    def _get_extra_files(self):
         return []
