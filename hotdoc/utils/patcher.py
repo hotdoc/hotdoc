293868113885f87f133ddc7367bf4e53d9aab5af
Mathieu Duponchelle <mathieu.duponchelle@opencreed.com>
2016-01-20 04:13:48 +0100

    patcher: pylint + pep8

--- hotdoc/utils/patcher.py
+++ hotdoc/utils/patcher.py
@@ -1,14 +1,26 @@
+"""
+Banana banana
+"""
 import codecs
 import commands
 
+
+# pylint: disable=too-few-public-methods
 class Patcher(object):
-    def patch (self, filename, begin, end, new_comment):
+    """
+    Banana banana
+    """
+    # pylint: disable=no-self-use
+    def patch(self, filename, begin, end, new_comment):
+        """
+        Banana banana
+        """
         file_encoding = commands.getoutput('file -b --mime-encoding %s' %
-                filename)
-        with codecs.open(filename, 'r', file_encoding) as f:
-            lines = f.readlines()
+                                           filename)
+        with codecs.open(filename, 'r', file_encoding) as _:
+            lines = _.readlines()
 
         res = lines[0:begin] + [new_comment + '\n'] + lines[end:]
         res = ''.join(res)
-        with open (filename, 'w') as f:
-            f.write(res)
+        with open(filename, 'w') as _:
+            _.write(res)
