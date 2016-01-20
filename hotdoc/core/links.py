22b9a2f9b85c68d99ca1a0416e7d1309f37bddc1
Mathieu Duponchelle <mathieu.duponchelle@opencreed.com>
2016-01-20 01:16:00 +0100

    links: pep8 + pylint

--- hotdoc/core/links.py
+++ hotdoc/core/links.py
@@ -1,10 +1,17 @@
+"""
+Banana banana
+"""
 from hotdoc.core.alchemy_integration import MutableObject
 from hotdoc.utils.simple_signals import Signal
 
 
-class Link (MutableObject):
+class Link(MutableObject):
+    """
+    Banana banana
+    """
     resolving_link_signal = Signal()
     resolving_title_signal = Signal()
+
     def __init__(self, ref, title, id_):
         self._title = title
         self.ref = ref
@@ -13,9 +20,12 @@ class Link (MutableObject):
 
     @property
     def title(self):
+        """
+        Banana banana
+        """
         resolved_title = Link.resolving_title_signal(self)
         resolved_title = [elem for elem in resolved_title if elem is not
-                None]
+                          None]
         if resolved_title:
             return resolved_title[0]
         return self._title
@@ -24,20 +34,30 @@ class Link (MutableObject):
     def title(self, value):
         self._title = value
 
-    def get_link (self):
+    def get_link(self):
+        """
+        Banana banana
+        """
         resolved_ref = Link.resolving_link_signal(self)
         resolved_ref = [elem for elem in resolved_ref if elem is not None]
         if resolved_ref:
             return resolved_ref[0]
         return self.ref
 
+
 class LinkResolver(object):
+    """
+    Banana banana
+    """
     def __init__(self, doc_tool):
         self.__links = {}
         self.__external_links = {}
         self.doc_tool = doc_tool
 
-    def get_named_link (self, name):
+    def get_named_link(self, name):
+        """
+        Banana banana
+        """
         if name in self.__links:
             return self.__links[name]
 
@@ -53,12 +73,18 @@ class LinkResolver(object):
         self.__links[name] = None
         return None
 
-    def add_link (self, link):
-        if not link.id_ in self.__links:
+    def add_link(self, link):
+        """
+        Banana banana
+        """
+        if link.id_ not in self.__links:
             self.__links[link.id_] = link
 
-    def upsert_link (self, link, overwrite_ref=False, external=False):
-        elink = self.__links.get (link.id_)
+    def upsert_link(self, link, overwrite_ref=False, external=False):
+        """
+        Banana banana
+        """
+        elink = self.__links.get(link.id_)
 
         if elink and external:
             return elink
@@ -73,5 +99,5 @@ class LinkResolver(object):
             if link.title is not None:
                 elink.title = link.title
             return elink
-        self.add_link (link)
+        self.add_link(link)
         return link
