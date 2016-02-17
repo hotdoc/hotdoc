b1fd912cdaac76cc8ab3ff1f21d2e6efba3b52e4
Mathieu Duponchelle <mathieu.duponchelle@opencreed.com>
2016-02-17 17:51:44 +0100

    0.6.9.4 release

--- setup.py
+++ setup.py
@@ -197,7 +197,7 @@ EXTRAS_REQUIRE = {
 }
 
 setup(name='hotdoc',
-      version='0.6.9.3',
+      version='0.6.9.4',
       description='A documentation tool micro-framework',
       keywords='documentation',
       url='https://github.com/hotdoc/hotdoc',
