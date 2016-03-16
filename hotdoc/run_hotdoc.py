3108d24ac7cab89416cfa0a56f063f03c9b969b0
Mathieu Duponchelle <mathieu.duponchelle@opencreed.com>
2016-03-16 19:39:31 +0100

    hotdoc script: put run function in separate module.

---
+++ hotdoc/run_hotdoc.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2016 Mathieu Duponchelle <mathieu.duponchelle@opencreed.com>
+# Copyright © 2016 Collabora Ltd
+#
+# This library is free software; you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation; either version 2.1 of the License, or (at your option)
+# any later version.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Banana banana
+"""
+
+import traceback
+
+from hotdoc.core.doc_repo import DocRepo
+from hotdoc.core.exceptions import HotdocException
+from hotdoc.utils.loggable import Logger
+
+
+def run(args):
+    """
+    Banana banana
+    """
+    res = 0
+    doc_repo = DocRepo()
+
+    # pylint: disable=broad-except
+    try:
+        doc_repo.setup(args)
+        doc_repo.format()
+        doc_repo.persist()
+    except HotdocException:
+        res = len(Logger.journal)
+    except Exception:
+        print ("An unknown error happened while building the documentation"
+               " and hotdoc cannot recover from it. Please report "
+               "a bug with this error message and the steps to "
+               "reproduce it")
+        traceback.print_exc()
+        res = 1
+    finally:
+        doc_repo.finalize()
+
+    return res
