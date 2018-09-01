1e84b1aa9524daccb702428b13cbef5204ba5efc
Thibault Saunier <tsaunier@igalia.com>
2018-09-01 18:25:48 -0300

    gtk_doc: Handle SECTION in .h

--- hotdoc/parsers/gtk_doc.py
+++ hotdoc/parsers/gtk_doc.py
@@ -93,8 +93,8 @@ class GtkDocParser:
             if not os.path.exists(section_name):
                 fname, ext = os.path.splitext(source_filename)
                 if self.__section_file_matching and os.path.exists(
-                        source_filename) and ext == ".c" and os.path.exists(
-                            fname + '.h'):
+                        source_filename) and ((ext == ".c" and os.path.exists(
+                            fname + '.h')) or ext == '.h'):
                     section_name = source_filename[:-2] + '.h'
                 else:
                     section_name = rel_filename
