a0e373c89d647a532ffc047cdc6a45a2f2d3fe2d
Thibault Saunier <tsaunier@igalia.com>
2019-06-14 17:52:58 -0400

    Revert "gi: Make sure to provide extra_attrs when rendering 'default' language"
    This reverts commit 256a6aeff9ce847aa16547a3391c3431f0134682.
    
    This is not the right fix

--- hotdoc/extensions/gi/gi_extension.py
+++ hotdoc/extensions/gi/gi_extension.py
@@ -947,7 +947,7 @@ class GIExtension(Extension):
         if link.ref is None:
             return GTKDOC_HREFS.get(link.id_)
 
-        return link.ref
+        return None
 
     def __translate_title(self, link, language):
         fund = FUNDAMENTALS[language].get(link.id_)
